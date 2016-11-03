1486861c4fb3aa28fa75b83d43d9502364b42484
Marianna-dl <marianna95220@gmail.com>
2016-11-03 15:10:30 +0100

    First commit

---
+++ src/main/java/deqo/mdel/App.java
@@ -0,0 +1,13 @@
+package deqo.mdel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
